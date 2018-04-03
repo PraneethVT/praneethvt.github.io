eb715e51cd6ec1d34a5d19ca2ecfddffe9339f10
Praneeth Thalla <pthalla1@gmail.com>
2018-04-02 21:12:43 -0400

    Moved arrow to its own component

---
+++ src/components/Arrow.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import Scroll from 'react-scroll';
+import '../css/Arrow.css'
+
+class Arrow extends React.Component {
+	constructor(props) {
+		super(props);
+		this.scrollToComponent = this.scrollToComponent.bind(this);
+		this.arrowProps = props.arrowProps;
+	}
+
+	componentDidMount() {
+		this.componentHeight = document.getElementById(this.arrowProps.scrollToComponentName).offsetTop;
+	}
+
+	scrollToComponent() {
+		Scroll.animateScroll.scrollTo(this.componentHeight, {
+			duration: 1000,
+			smooth: "easeOutQuint"
+		});
+	}
+
+	render() {
+
+		let transformation = (this.arrowProps.direction === "up") ? "rotate(225deg)": "rotate(45deg)";
+		let arrowStyle = {
+			"border-style": this.arrowProps.borderStyle,
+			"border-color": this.arrowProps.borderColor,
+			"transform": transformation,
+			"-webkit-transform": transformation
+		}
+
+		return(
+	    <p id="bounceholder" onClick={this.scrollToComponent}><i id="arrow" style={arrowStyle}></i></p>
+		);
+	}
+}
+
+export default Arrow;
